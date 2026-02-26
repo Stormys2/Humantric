5e145fbb675f2afd25b889265719d16a8c161fe1
Stormys2 <105914034+Stormys2@users.noreply.github.com>
2026-02-26 16:44:25 -0400

    api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://humantric.up.railway.app/'; // se actualizará cuando hagamos el backend
+const API_URL = 'https://humantric.up.railway.app'; // se actualizará cuando hagamos el backend
 
 const api = axios.create({ baseURL: API_URL });
 
